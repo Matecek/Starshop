30c9dda16d12e1ef50f65dadf1c0e9ae3d1c5e90
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2025-06-24 12:47:24 +0200

    Config

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,7 +8,8 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController
 {
     #[Route('/')]
-    public function homePage() : Response {
+    public function homePage(): Response
+    {
         return new Response('<strong>Starshop</strong>: your monopoly-busting option for Starship parts!');
     }
 }
