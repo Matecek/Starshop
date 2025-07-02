afa569683ea1af82a3bd76765fb400765211acea
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2025-07-02 15:37:44 +0200

    services

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Model\Starship;
+use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,31 +10,9 @@ use Symfony\Component\Routing\Attribute\Route;
 class StarshipApiController extends AbstractController
 {
     #[Route('/api/starships')]
-    public function getCollection(): Response
+    public function getCollection(StarshipRepository $repository): Response
     {
-        $starships = [
-            new Starship(
-                1,
-                'USS LeafyCruiser (NCC-0001)',
-                'Garden',
-                'Jean-Luc Pickles',
-                'under construction'
-            ),
-            new Starship(
-                2,
-                'USS VeggieVoyager (NCC-0002)',
-                'Explorer',
-                'Kathryn Carrot',
-                'active'
-            ),
-            new Starship(
-                3,
-                'USS TomatoTrek (NCC-0003)',
-                'Research',
-                'Benjamin Broccoli',
-                'decommissioned'
-            ),
-        ];
+        $starships = $repository->findAll();
 
         return $this->json($starships);
     }
